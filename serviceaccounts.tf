b81577b1315ecb27f65df22d9495486708f5cc56
Tor-zeR <143712049+Tor-zeR@users.noreply.github.com>
2025-03-10 16:55:07 +0300

    Update serviceaccounts.tf

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -2,13 +2,13 @@ resource "google_service_account" "service_accounts" {
   for_each        = toset(["dev", "qa", "stage", "prod"])
   account_id      = "viewer-sa-${each.key}"
   display_name    = "Viewer Service Account for ${each.key}"
-  project         = var.project_id
+  project         = var.project-id
 }
 
 resource "google_project_iam_member" "project_roles" {
   for_each = toset(["dev", "qa", "stage", "prod"])
 
-  project = var.project_id
+  project = var.project-id
   role    = "roles/viewer"
   member  = "serviceAccount:${google_service_account.service_accounts[each.key].email}"
 }
